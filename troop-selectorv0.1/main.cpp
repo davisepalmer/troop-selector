c02dbaa041df75a2d290e2495b6ec409e61b025a
Davis Palmer <davisepalmer@gmail.com>
2023-12-19 16:11:06 -0600

    Finished 0.2. See bug list

--- troop-selectorv0.1/main.cpp
+++ troop-selectorv0.1/main.cpp
@@ -1,16 +1,17 @@
-// troop-selectorv0.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// troop-selectorv0.1.cpp : This file contains the 'main' function. Program execution begins and ends there. No headers for this btw 
+// x64
 
-
-// VERSION 0.1
+// VERSION 0.2
 /*
-Feature list on 0.2:
+FIX: Skipping heroes sometimes (weird spacing), when on heroes activating abilities <- check personal notes for solution
+Feature list on 0.3:
     - Click (0) (1) (0) for average spacing between troop
     - Memory of ^^ between closing
-0.3+:
+0.4+:
     - GUI
     - Retained memory
-    - Autosearching (1m+ GC || EXL) (maybe different exe)
+    - Scroll wheel and 'a' or 'd' instead of just arrow keys, maybe up and down
+    - If player clicks in y = troop bar +- 50 pixels the current location changes to that
 */
 
 #include <iostream>
@@ -18,11 +19,62 @@ Feature list on 0.2:
 #include <conio.h>
 
 struct AttackSelector{
-    int initXPos = 0; // troop 1(0) x pos
-    int initYPos = 0; // troop 1(0) y pos
-    int spacing = 0; // spacing between troops
+    long initXPos = 100;
+    long xPos = 100; // troop 1(0) x pos
+    long yPos = 100; // troop 1(0) y pos
+    long spacing = 200; // spacing between troops
     bool attacking = 0; // has attack started t/f
-    void moveTroop(char dir) {}; // Moves troop right or left // feature: typedefs for abstraction with DIRECTION: L R
+    bool hasInitPos = 0;
+    bool initPos() { // inits our troop xPos and yPos returns t/f bsed off if set yet
+        std::cout << "Click the center of the first troop, second, then first again please." << std::endl;
+        // on click set as xPos
+        // on second click set difference as spacing
+        size_t i = 0;
+        while (1) {
+            POINT z;
+            GetCursorPos(&z);
+            if (GetAsyncKeyState(VK_LBUTTON) && i == 0) {
+                this->xPos = this->initXPos = z.x;
+                this->yPos = z.y;
+                i++;
+                Sleep(100);
+            }
+            else if (GetAsyncKeyState(VK_LBUTTON) && i == 1) {
+                this->spacing = z.x - this->xPos;
+                i++;
+                Sleep(100);
+            }
+            else if (GetAsyncKeyState(VK_LBUTTON && i >= 2)) {
+                this->hasInitPos = 1;
+                return 1;
+            }
+        }
+        return 0;
+    }; 
+    void moveTroop(char dir/*, POINT currp */ ) { // Moves troop right or left // feature: typedefs for abstraction with DIRECTION: L R
+        std::cout << "Begun moveTroop()" << std::endl;
+        INPUT troopInput = { 0 };
+        troopInput.type = INPUT_MOUSE;
+        troopInput.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+        switch (dir) {
+            case 'L':
+                this->xPos -= spacing;
+                SetCursorPos(this->xPos, this->yPos);
+                break;
+            case 'R':
+                this->xPos += spacing;
+                SetCursorPos(this->xPos, this->yPos);
+                break;
+        }
+
+        SendInput(1, &troopInput, sizeof(troopInput));
+        ZeroMemory(&troopInput, sizeof(troopInput));
+        troopInput.type = INPUT_MOUSE;
+        troopInput.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+        SendInput(1, &troopInput, sizeof(troopInput));
+        Sleep(10);
+        //SetCursorPos(currp.x, currp.y);
+    };
     void attackFinished() {}; // Clears all init properties // feature: auto does it based off of ending screen or troop bar going away
 };
 
@@ -36,9 +88,13 @@ int main()
     while (1) {
         LPCWSTR windowName = L"Clash of Clans";
         HWND hWND = FindWindow(NULL, windowName);
-        while (hWND == NULL) {
+        while (hWND == NULL ) {
             hWND = FindWindow(NULL, windowName);
-            std::cout << "Have not found CoC" << std::endl;
+            if (hWND != NULL)
+                std::cout << "Found CoC" << std::endl;
+            else
+                std::cout << "Have not found CoC" << std::endl;
+            Sleep(100);
         }
         
         POINT p;
@@ -50,28 +106,36 @@ int main()
             newAttack.attacking = 1;
         }
 
-        if (newAttack.attacking) { 
+        if (newAttack.attacking && !newAttack.hasInitPos) {
+            // init Troop Locs
+            newAttack.initPos();
+        }
+        if (newAttack.attacking && newAttack.hasInitPos) {
             if (GetAsyncKeyState(VK_LEFT)) { // LEFT ARROW
                 std::cout << "LArrow Pressed" << std::endl;
-                newAttack.moveTroop('L');
-                Sleep(250);
+                newAttack.moveTroop ('L');
+                SetCursorPos(p.x, p.y);
+                Sleep(150);
 
             }
             if (GetAsyncKeyState(VK_RIGHT)) { // RIGHT ARROW
                 std::cout << "RArrow Pressed" << std::endl;
                 newAttack.moveTroop('R');
-                Sleep(250);
+                SetCursorPos(p.x, p.y);
+                Sleep(150);
             }
+             // send back to original location
         }
 
         if (GetAsyncKeyState(VK_ESCAPE)) { // ESCAPE
             std::cout << "ESC Pressed" << std::endl;
+            newAttack.attackFinished();
             std::cout << "Killing Program" << std::endl;
             Sleep(100);
             return 0;
         }
         
-    } //end while(1)
+    } //end big while
 
     std::cout << "Outside End." << std::endl;
     return 0;
